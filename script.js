a9d6a1f42ca9065607568bf1c7f3a7f8ad105e89
FONSEKX <kasta7@outlook.com>
2022-05-19 12:43:14 -0500

    fonsekx
    mi primer

---
+++ script.js
@@ -0,0 +1,10 @@
+const fulImgBox = document.getElementById("fulImgBox"),
+fulling = document.get.ElementById("fulImg");
+
+function openFulImg(reference){
+fulImgBox.style.display = "flex";
+fulImg.src = reference
+}
+function closeImg(){
+fulImgBox.style.display = "none";
+}
